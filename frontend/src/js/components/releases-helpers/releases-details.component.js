730a8cc14320533caa8e0a5b76d2c9dc2c50a2f7
Joel <jrevertvila@gmail.com>
2020-10-13 11:48:55 +0200

    buttons for ownwer release/tweet

--- frontend/src/js/components/releases-helpers/releases-details.component.js
+++ frontend/src/js/components/releases-helpers/releases-details.component.js
@@ -1,7 +1,19 @@
 class ReleasesDetailsCtrl {
-    constructor($scope, $state){
+    constructor($scope, $state, User, Releases){
         "ngInject";
+        this._User = User;
+        this._Releases = Releases;
+        this._$state = $state;
         this._$scope = $scope;
+        this.$onInit = function () {
+            if (this._User.current) this.canModify = (this._User.current.username == this.release.author.username ? true : false);
+        };
+    }
+
+    removeRelease(){
+        this._Releases.destroy(this.release.slug).then(() => {
+            this._$state.go('app.releases')
+        })
     }
 }
 
--- frontend/src/js/components/tweets-helpers/tweet-preview.component.js
+++ frontend/src/js/components/tweets-helpers/tweet-preview.component.js
@@ -1,10 +1,24 @@
 class TweetPreviewCtrl {
-    constructor($scope, $state, User){
+    constructor($scope, $state, User, Tweets) {
         "ngInject";
         this._$scope = $scope;
         // this._Tweets = Tweets;
         this._User = User;
+        this._Tweets = Tweets;
+        this.$onInit = function () {
+            if (this._User.current) this.canModify = (this._User.current.username == this.tweet.author.username ? true : false);
+        };
+
     }
+
+    deleteTweet() {
+        console.log("hola");
+        // console.log();
+        this._Tweets.destroy(this.tweet.slug).then(() => {
+            location.reload();
+        })
+    }
+
 }
 
 let TweetPreview = {
