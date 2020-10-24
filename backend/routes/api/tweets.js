aac9aab58037fd19c7530665a51c3aa0f443ec03
jrevertvila <jrevertvila@gmail.com>
2020-10-24 12:06:59 +0200

    remove tweet parent and possible replies of replies working

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -178,52 +178,34 @@ router.get('/:tweet', auth.optional, function (req, res, next) {
 
 let getTweetByObjID = (id) => {
   let tweet;
-  Tweet.findOne({ _id: id }).then((data)=>{
+  Tweet.findOne({ _id: id }).then((data) => {
     tweet = data;
     console.log(data);
   });
   return tweet;
 }
 
-function addToProvisional() {
-
-}
-
 //DELETE TWEET BY SLUG
 router.delete('/:tweet', auth.required, function (req, res, next) {
-  User.findById(req.payload.id).then( async function (user) {
+  User.findById(req.payload.id).then(async function (user) {
     if (!user) { return res.sendStatus(401); }
 
     if (req.tweet.author._id.toString() === req.payload.id.toString()) {
-
-      let arrToDelete = [];
-      let arrProvisional = [];
-
-      if (req.tweet.replies.length !== 0) {
-        console.log("TIENE REPLIES ===================================");
-        let childs = true;
-        let ArrReplies = req.tweet.replies;    
-        arrProvisional.concat(ArrReplies);
-
-        for (let x = 0; x < arrProvisional.length; x++) {
-
-          let obj = arrProvisional[x].slug ? arrProvisional[x]._id : arrProvisional[x];
-
-          Tweet.findOne({_id : obj}).then((data) => {
-            if (data.replies) {
-              
-            }
-          })
-          
-        }
-
-
-      } else {
-        console.log("NO TIENE REPLIES");
+      let arrDelete = [];
+      arrDelete = [req.tweet._id];
+      for (let x = 0; x < arrDelete.length; x++) {
+        let obj = arrDelete[x].slug ? arrDelete[x]._id : arrDelete[x];
+        await Tweet.findOne({ _id: obj }).then((data) => {
+          if (data.replies.length !== 0) {
+            arrDelete = [...arrDelete, ...data.replies];
+          }
+        })
       }
-      // return req.tweet.remove().then(function () {
-      //   return res.sendStatus(204);
-      // });
+      Tweet.remove({ _id: { $in: arrDelete } }).then((result) => {
+        return res.status(200).send('Deleted correctly');
+      }).catch(() => {
+        return res.sendStatus(503)
+      });
     } else {
       return res.sendStatus(403);
     }
